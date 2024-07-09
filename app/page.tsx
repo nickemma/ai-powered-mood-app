0c8dbd0749dcb5d697c97651f4d9ad6c2b1f788b
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-07-09 11:59:31 +0100

    Homepage made responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ export default function Home() {
   return (
     <section className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] max-auto">
-        <h1 className="text-5xl mb-4">AI-Powered Mood Journal App</h1>
-        <p className="text-[1.25rem] text-white/60 mb-4">
+        <h1 className="text-2xl mb-4 mx-1 xs:text-4xl sm:text-5xl">
+          AI-Powered Mood Journal App
+        </h1>
+        <p className="text-[1rem] mx-1  text-white/60 mb-4 xs:text-2xl">
           Empower yourself with a deeper understanding of your emotional
           well-being and take steps towards a happier, healthier life.
         </p>
         <div className="transition-all">
           <Link href="/journal">
-            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl hover:bg-blue-800 transition-all">
+            <button className="bg-blue-600 mx-1 px-2 py-2 rounded-lg text-base hover:bg-blue-800 transition-all xs:px-4 xs:py-2 xs:text-xl">
               Get Started
             </button>
           </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        'xs': '375px', 
+        'sm': '640px', 
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
     },
   },
