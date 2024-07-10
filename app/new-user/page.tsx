7f06d9071b983163cd3ba5879696d7483c37dd5a
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-07-10 02:58:50 +0100

    fixed email error

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,24 @@
-const NewUser = () => {
+import { prisma } from '@/utils/db'
+import { auth, EmailAddress } from '@clerk/nextjs/server'
+
+const createNewUser = async () => {
+  const { userId } = await auth()
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: userId!,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: userId!,
+        email: (await EmailAddress.get(userId!))?.email,
+      },
+    })
+  }
+}
+export const NewUser = async () => {
+  await createNewUser()
   return <h2>hello</h2>
 }
